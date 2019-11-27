a3d3d6a7e8fd84aa38f79072aca82dd38a25d5e9
Fachrz <rfachru3@outlook.com>
2019-11-27 09:42:47 +0700

    First Commit

---
+++ app/Http/Controllers/Home_controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Home_controller extends Controller
+{
+    public function index(){
+        return view('liang_homepage');
+    }
+}
