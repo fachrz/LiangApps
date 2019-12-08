e60564232bef22a00e04388a2fb677d03b8ed264
fachrz <rfachru3@outlook.com>
2019-12-08 22:17:53 +0700

    remove unused code

--- resources/views/layouts/without_navbar.blade.php
+++ resources/views/layouts/without_navbar.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/bootstraps/css/bootstrap.min.css') }} ">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
 
@@ -22,8 +21,6 @@
     @yield('content')
 
     <!-- JavaScript Library -->
-    <script src="{{ asset('js/core.js') }}"></script>
-    <script src="{{ asset('js/style.js') }}"></script>
     @yield('script')
 </body>
 
